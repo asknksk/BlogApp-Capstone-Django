4c3715d730a92d56dc7d14c9f05a2523378ceffc
Askin <asknkskk@gmail.com>
2022-10-16 14:31:47 +0300

    users created

---
+++ users/serializers.py
@@ -0,0 +1,103 @@
+# serializers.py
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth.password_validation import validate_password
+from dj_rest_auth.serializers import TokenSerializer
+from .models import Profile
+
+
+class RegistrationSerializer(serializers.ModelSerializer):
+    email = serializers.EmailField(
+            required=True,
+            validators=[UniqueValidator(queryset=User.objects.all(), # unique email
+            message='This Email has been used.')] 
+            )
+
+    password = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password], 
+        style={"input_type": "password"} 
+    )
+
+    password2 = serializers.CharField(
+        write_only=True,
+        required=True,
+        validators=[validate_password],
+        style={"input_type": "password"}
+    )
+
+    class Meta:
+        model = User
+        fields = ('username', 'password', 'password2', 'email', 'first_name', 'last_name')
+        extra_kwargs = {
+            'first_name': {'required': True},
+            'last_name': {'required': True}
+        }
+
+    def validate(self, attrs): # validating if password and password2 match
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data): 
+        user = User.objects.create(
+            username=validated_data['username'],
+            email=validated_data['email'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name']
+        )
+
+        
+        user.set_password(validated_data['password']) 
+        user.save()
+
+        return user
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email'
+        )
+
+
+class CustomTokenSerializer(TokenSerializer):
+    user = UserSerializer(read_only=True)
+
+    class Meta(TokenSerializer.Meta):
+        fields = (
+            'key',
+            'user'
+        )
+
+class ProfileUpdateForm(serializers.ModelSerializer): 
+    user = UserSerializer()
+    class Meta:
+        model = Profile 
+        fields = (
+            'image',
+            'user'
+        )
+
+    def update(self, instance, validated_data):
+        user_data = validated_data.pop('user')
+        user = instance.user
+
+        instance.image = validated_data.get('image', instance.image)
+        instance.save()
+
+        user.username = user_data.get(
+            'username',
+            user.username
+        )
+        user.email = user_data.get(
+            'email',
+            user.email
+         )
+        user.save()
+
+        return instance
--- users/views.py
+++ users/views.py
@@ -3,7 +3,7 @@ from rest_framework import generics, status
 from django.contrib.auth.models import User
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
-from .serializers import RegistrationSerializer
+from .serializers import RegistrationSerializer, ProfileUpdateForm
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, RetrieveAPIView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Profile
@@ -18,8 +18,27 @@ class RegisterView(generics.CreateAPIView):
         serializer.is_valid(raise_exception=True)
         user = serializer.save()
         data = serializer.data
-        token = Token.objects.get(user=user)
-        data['token'] = token.key
+        if Token.objects.filter(user=user).exists():
+            token = Token.objects.get(user=user)
+            data['token'] = token.key
+        else:
+            data['error'] = 'User dont have token. Please login'
         headers = self.get_success_headers(serializer.data)
         return Response(data, status=status.HTTP_201_CREATED, headers=headers)
 
+
+class ProfileView(LoginRequiredMixin, RetrieveUpdateDestroyAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileUpdateForm
+
+    def get_queryset(self):
+        return self.queryset.all()
+
+class OnlyRet(RetrieveAPIView):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileUpdateForm
+
+    def get(self, request, *args, **kwargs):
+        print(self.request)
+        print(self.get_object())
+        return self.retrieve(request, *args, **kwargs)
